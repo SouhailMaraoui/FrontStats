6051be86a27153f486af180baf1a36f3b618bd0a
Souhail Maraoui <m.souhail2@outlook.com>
2020-05-27 01:18:41 +0000

    Show list of Cooperatives in table

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
+import {Table, Button} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          First Commit
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    cooperatives:[]
+  }
+
+  componentWillMount() {
+    axios.get('http://costats.ew.r.appspot.com/cooperative/all/')
+        .then((response) =>{
+      this.setState({
+          cooperatives: response.data
+      })
+    });
+  }
+
+  render() {
+    let cooperatives = this.state.cooperatives.map((cooperative)=>{
+      return (
+          <tr key={cooperative.id}>
+            <td>{cooperative.id}</td>
+            <td>{cooperative.nomCooperative}</td>
+            <td>{cooperative.motDePasse}</td>
+            <td>
+              <Button color="success" size='sm'>Update</Button>
+              <Button color="danger" size='sm'>Delete</Button>
+            </td>
+          </tr>
+      )
+    });
+    return (
+        <div className="App container">
+          <Table>
+            <thead>
+            <tr>
+              <th>#</th>
+              <th>nom Cooperative</th>
+              <th>mot De Passe</th>
+              <th>Actions</th>
+            </tr>
+            </thead>
+
+            <tbody>
+                {cooperatives}
+            </tbody>
+          </Table>
+        </div>
+    );
+  }
 }
 
 export default App;
